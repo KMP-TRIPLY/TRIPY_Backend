a0a6980af2b2498cdadfd375e5e835f33668ca71
shinhee818 <xbox234@naver.com>
2026-08-03 11:04:42 +0900

    Revert "feat: 게임 플레이 에러코드 및 진행 조회 리포지토리 추가"

--- src/main/java/com/kmp/Triply/domain/game/repository/MissionAttemptRepository.java
+++ src/main/java/com/kmp/Triply/domain/game/repository/MissionAttemptRepository.java
@@ -1,7 +1,5 @@
 package com.kmp.Triply.domain.game.repository;
 
-import com.kmp.Triply.domain.game.entity.AttemptResult;
-import com.kmp.Triply.domain.game.entity.AttemptType;
 import com.kmp.Triply.domain.game.entity.MissionAttempt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,14 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MissionAttemptRepository extends JpaRepository<MissionAttempt, Long> {
-
-    boolean existsByGameProgressIdAndMissionIdAndResult(Long gameProgressId, Long missionId, AttemptResult result);
-
-    boolean existsByGameProgressIdAndMissionIdAndAttemptType(Long gameProgressId, Long missionId, AttemptType attemptType);
-
-    long countByGameProgressIdAndResult(Long gameProgressId, AttemptResult result);
-
-    List<MissionAttempt> findAllByGameProgressIdAndResult(Long gameProgressId, AttemptResult result);
 
     @Query("""
             select coalesce(sum(missionAttempt.scoreEarned), 0)
