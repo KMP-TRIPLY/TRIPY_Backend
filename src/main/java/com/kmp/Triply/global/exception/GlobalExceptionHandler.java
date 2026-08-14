a322dca40fee14a5c9fc8570d44c325d659f860c
shinhee818 <xbox234@naver.com>
2026-08-14 10:45:09 +0900

    fix: 잘못된 요청이 500으로 새던 예외 핸들러 추가

--- src/main/java/com/kmp/Triply/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kmp/Triply/global/exception/GlobalExceptionHandler.java
@@ -11,7 +11,9 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
